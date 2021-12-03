931b0222aa479b68b2553f1c1e8a5ae44096463f
jamesa <james.aslett@directlife.co.uk>
2021-12-03 13:36:25 +0000

    Add project files.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Advent-of-Code
+This repository contains my solutions to <a href="https://adventofcode.com/" target="_blank">Advent of Code</a> 2021 challenges.
+
+# Stars count: 6 :star:
+
+Day | Task | Solution | STARS |
+------------ | ------------ | ------------- | ------------- |
+1 |[Sonar Sweep](./day-1) |[day 1 solution](./day-1/Program.cs) | :star: :star: |
---
+++ day-1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "puzzleInput.txt"));
+            int[] report = (Array.ConvertAll(input, s => Int32.Parse(s)));
+
+            Console.WriteLine("########## Day 1 2021 ##########");
+            Console.WriteLine($"Part one solution: {SolvePartOne(report)}");
+            Console.WriteLine($"Part two solution: {SolvePartTwo(report)}");
+            Console.WriteLine("################################");
+        }
+
+        private static int SolvePartOne(int[] report)
+        {
+            int increaseNumber = 0;
+            for (int i = 1; i < report.Count(); i++)
+            {
+                if (report[i] > report[i - 1])
+                {
+                    increaseNumber++;
+                }
+            }
+            return increaseNumber;
+        }
+
+        private static int SolvePartTwo(int[] report)
+        {
+            int increaseNumber = 0;
+
+            for (int i = 1; i < report.Count() - 2; i++)
+            {
+                int currentOne = report[i - 1] + report[i] + report[i + 1];
+                int nextOne = report[i] + report[i + 1] + report[i + 2];
+
+                if (nextOne > currentOne)
+                {
+                    increaseNumber++;
+                }
+            }
+            return increaseNumber;
+        }
+    }
+}
