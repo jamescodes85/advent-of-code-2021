524572b4b66dabd0d0493298177c696788487953
James Aslett <JamesA@directlife.co.uk>
2021-12-13 11:36:35 +0000

    Updates

--- day-3/Program.cs
+++ day-3/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
-namespace day_3
+namespace day_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] input = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "puzzleInput.txt"));
+
+            Console.WriteLine("########## Day 3 2021 ##########");
+            //Console.WriteLine($"Part one solution: {SolvePartOne(input)}");
+            Console.WriteLine($"Part two solution: {SolvePartTwo(input)}");
+            Console.WriteLine("################################");
+        }
+
+        private static int SolvePartOne(string[] input)
+        {
+        //for each binary number
+            //GAMMA: get most common number from each of the 5 bits and from that create new binary, then covert to demical
+
+            //EPSILON: same as above but for least commmon
+
+            //GAMMA * EPSILON = power consumptions;
+        }
+
+        private static int SolvePartTwo(string[] input)
+        {
         }
     }
 }
